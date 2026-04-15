2483ba1c4ed87ce3201b853380f61db053aeb541
SakuyaMeS <2952704112@qq.com>
2026-04-15 08:48:07 +0800

    fix tuple space spelling and indentation errors

--- tuple_space.py
+++ tuple_space.py
@@ -1,28 +1,28 @@
 import threading
 
-class TupleSpcace:
+class TupleSpace:
     def _init_(self):
         self.data = {}
         self.lock = threading.Lock
 
-        def read(self, key):
-            with self.lock:
-                if key in self.data:
-                    return True, self.data[key]
-                else: return False, None
+    def read(self, key):
+        with self.lock:
+            if key in self.data:
+                return True, self.data[key]
+            else: return False, None
 
-        def get(self, key):
-            with self.lock:
-                if key in self.data:
-                    value = self.data[key]
-                    del self.data[key]
-                    return True, value
-                else: return False, None
+    def get(self, key):
+        with self.lock:
+            if key in self.data:
+                value = self.data[key]
+                del self.data[key]
+                return True, value
+            else: return False, None
 
-        def put(self, key, value):
-            with self.lock:
-                if key in self.data:
-                    return False
-                else: 
-                    self[key] = value
-                    return True
+    def put(self, key, value):
+        with self.lock:
+            if key in self.data:
+                return False
+            else: 
+                self[key] = value
+                return True
