133a299ab218e2045a1a5c0af127d339981fdf18
SakuyaMeS <2952704112@qq.com>
2026-04-15 08:22:28 +0800

    add tuple space class skeleton

---
+++ tuple_space.py
@@ -0,0 +1,15 @@
+import threading
+
+class TupleSpcace:
+    def _init_(self):
+        self.data = {}
+        self.lock = threading.Lock
+
+        def read(self, key):
+            pass
+
+        def get(self, key):
+            pass
+
+        def put(self, key, value):
+            pass
