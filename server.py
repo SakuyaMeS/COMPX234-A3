6a45c013aeff6bf5dcf7f6fd4ff878c9a61ee5ca
SakuyaMeS <2952704112@qq.com>
2026-04-22 08:44:31 +0800

    add receive_n helper for server socket reads

--- server.py
+++ server.py
@@ -11,3 +11,12 @@ get_count = 0
 put_count = 0
 error_count = 0
 lock = threading.Lock()
+
+def receive_n(sock, num_bytes):
+    data = b""
+    while len(data) < num_bytes:
+        chunk = sock.recv(num_bytes - len(data))
+        if not chunk:
+            break
+        data += chunk
+    return data
