ad552d35d206499067083086449117dabf387da5
SakuyaMeS <2952704112@qq.com>
2026-04-23 07:50:00 +0800

    add client main setup and input file validation

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python tuple_space_client.py <server-hostname> <server-port> <input-file>")
+        sys.exit(1)
+    
+    hostname = sys.argv[1]
+    port = int(sys.argv[2])
+    input_file_path = sys.argv[3]
+
+    if not os.path.exists(input_file_path):
+        print(f"Error: Input file '{input_file_path}' does not exist.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
